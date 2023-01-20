97c4ee417ba0e9fa91b7f7a90a76500b03d93668
gargshubham55 <gargshubham55@gmail.com>
2023-01-20 18:22:06 +0530

    End of Section 2

---
+++ SGWalks/SGWalks.API/Data/SGWalksContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGWalks.API.Models.Domain;
+
+namespace SGWalks.API.Data
+{
+    public class SGWalksContext : DbContext
+    {
+        public SGWalksContext(DbContextOptions<SGWalksContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDificulty { get; set; }
+    }
+}
---
+++ SGWalks/SGWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,18 @@
+﻿namespace SGWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+
+        public Double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        // Navigation 
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }    
+    }
+
+}
